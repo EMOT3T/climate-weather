e195a11d9762c8004f756ab813a97093a748fe33
Samuel Torres <158462704+EMOT3T@users.noreply.github.com>
2024-03-19 09:21:32 -0300

    Update script.js
    modify the code to English

--- climate-weather/js/script.js
+++ climate-weather/js/script.js
@@ -1,44 +1,44 @@
 const API_KEY = "4e63b730d1e97486e34a23103d40cb66";
 
-function atualizarValores(data) {
-  document.getElementById("cityText").textContent = `Clima em ${data.name}`;
+function updateValues(data) {
+  document.getElementById("cityText").textContent = `Climate in ${data.name}`;
   document.getElementById("temp").textContent = `${Math.floor(data.main.temp)}°C`;
   document.getElementById("weatherText").textContent = data.weather[0].description;
-  document.getElementById("humidity").textContent = `Humidade: ${data.main.humidity}%`;
+  document.getElementById("humidity").textContent = `Humidity: ${data.main.humidity}%`;
   document.getElementById("searchImg").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 }
 
-async function buscarCidade(cidade) {
+async function searchCity(city) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&lang=pt_br&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=pt_br&units=metric`
     );
     if (!response.ok) {
-      throw new Error(`Erro ao buscar dados: ${response.status}`);
+      throw new Error(`Error when fetching data : ${response.status}`);
     }
     const data = await response.json();
-    atualizarValores(data);
+    updateValues(data);
   } catch (error) {
     console.error(error.message);
-    mostrarMensagemErro(error.message);
+    errorMessage(error.message);
   }
 }
 
-function pesquisar() {
-  const cidade = document.getElementById("inputCity").value.trim();
-  if (!cidade) {
+function search() {
+  const city = document.getElementById("inputCity").value.trim();
+  if (!city) {
     return;
   }
-  buscarCidade(cidade);
+  searchCity(city);
 }
 
 document.getElementById("inputCity").addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    pesquisar();
+    search();
   }
 });
 
-function mostrarMensagemErro(mensagem) {
+function errorMessage(message) {
 }
 
-buscarCidade("São Paulo");
+searchCity("São Paulo");
